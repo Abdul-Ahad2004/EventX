f00ac5f03a3ddd9432f61870255ead71151888bf
Abdul Ahad Tariq <abdulahadg2004@gmail.com>
2024-07-26 16:48:52 +0500

    User model added

--- models/user.js
+++ models/user.js
@@ -1,1 +1,31 @@
-import mongoose from "mongoose";
+import mongoose,{Schema} from "mongoose";
+
+const userschema =new Schema(
+    {
+      username:{
+        type:String,
+        required:true,
+        unique:true,
+      },
+      email:{
+        type: String,
+        required:true,
+        unique:true,
+      },
+      password:{
+        type:String,
+        required:true,
+      },
+      age:{
+        type:Number,
+        required:true,
+
+      },
+      PhoneNumber:{
+        type:String,
+        required:true,
+      }
+    }
+)
+
+export const User=mongoose.model("User",userschema)
