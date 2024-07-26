f30c09ad94aacf9191667d1807a73413a93aa449
Abdul Ahad Tariq <abdulahadg2004@gmail.com>
2024-07-26 20:03:24 +0500

    Folder structure updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv"
-import authRouter from "./routes/auth.js"
 import userRouter from "./routes/user.js"
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connect.js";
@@ -13,7 +12,6 @@ const PORT = process.env.PORT || 3000
 connectDB()
 app.use(express.json())
 app.use(cookieParser())
-app.use("/auth/user", authRouter);
 app.use("/user", userRouter);
 
 app.listen(PORT, () => {
