658fd3baa69ce872a319b0e2378a143776951d0c
Abdul Ahad Tariq <abdulahadg2004@gmail.com>
2024-08-04 16:18:55 +0500

    Post Event page added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [events, setevents] = useState([]);
   const [applicantNumbers, setApplicantNumbers] = useState({});
+  const Navigate = useNavigate();
   async function getApplicantsNumber(eventId) {
     try {
       const response = await axios.get(
@@ -62,16 +64,17 @@ function Home() {
       <Navbar />
       <div className="flex flex-col justify-center gap-6">
         <div className="flex justify-center items-center m-4">
-          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Post New event</button>
+          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          onClick={()=>{Navigate('/PostEvent')}}>Post New event</button>
         </div>
         <div>
           <h1 className="mb-2 text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">Your Events</h1>
           {events?.length === 0 ? (
             <div className="mb-3 font-normal text-gray-700 "> No Events posted by You</div>
           ) : (
-            <div className=" ml-2 max-w-sm p-6 bg-white border flex flex-wrap gap-2 border-gray-200 rounded-lg shadow">
+            <div className="flex flex-wrap gap-2 ">
               {events.map((event) => (
-                <div key={event._id} >
+                <div key={event._id} className="ml-2 max-w-sm p-6 bg-white border  border-gray-200 rounded-lg shadow" >
                   <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{event.eventType}</h2>
                   <div className="flex flex-col gap-1">
                     <div className="mb-3 font-normal text-gray-700">Event date: {getdate(event.date)}</div>
