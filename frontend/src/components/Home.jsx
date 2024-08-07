48158c9fb75c4344ee045156402671eab28e77e9
Abdul Ahad Tariq <abdulahadg2004@gmail.com>
2024-08-07 22:10:09 +0500

    Manage tasks functionality done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -195,7 +195,12 @@ function Home() {
                           Having an assigned Planner
                         </div>
                         <div className="flex gap-1">
-                          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                           onClick={() => {
+                            Navigate("/ManageTasks", {
+                              state: { eventId: event._id },
+                            });
+                          }}>
                             Manage Event Tasks
                           </button>
                           <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
